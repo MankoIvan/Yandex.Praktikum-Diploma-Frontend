42d0ae7d621363f439cce21a491f9109649da449
Manko Ivan <inoi2002@gmail.com>
2020-02-16 03:17:52 +0300

    search fix1

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -154,12 +154,12 @@ function bindHandlers() {
                 if (res.articles.length) {
                     mainApi.getUser()
                         .then(res1 => {
-                            newsCardList.renderFromSearch(res.articles, searchInput.value, true);
+                            newsCardList.renderFromSearch(res.articles, searchInput.value, false);
                             reslutLoading.classList.remove("result__loading_opened");
                             resultExist.classList.add("result__exist_opened");
                         })
                         .catch(err => {
-                            newsCardList.renderFromSearch(res.articles, searchInput.value, false);
+                            newsCardList.renderFromSearch(res.articles, searchInput.value, true);
                             reslutLoading.classList.remove("result__loading_opened");
                             resultExist.classList.add("result__exist_opened");
                         })
@@ -175,10 +175,10 @@ function bindHandlers() {
     showMoreButton.addEventListener('click', function() {
         mainApi.getUser()
             .then(res => {
-                newsCardList.renderSomeCards(true);
+                newsCardList.renderSomeCards(false);
             })
             .catch(res => {
-                newsCardList.renderSomeCards(false);
+                newsCardList.renderSomeCards(true);
             })
     });
     
